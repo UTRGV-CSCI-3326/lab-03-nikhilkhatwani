9f8a2214b9fc15ddcf0e1c8bed465df57da512d0
Nikhil Khatwani <85192316+nikhilkhatwani@users.noreply.github.com>
2025-01-30 21:17:44 +0000

    initial commit

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,16 @@
+public class BankAccount {
+    public static void main(String[] args) {
+        float bobsBalance = 2175.370f;
 
+        System.out.println("Bob's old balance: " + bobsBalance);
+
+        bobsBalance -= 302.50;
+        bobsBalance += 50.03;
+        bobsBalance -= (bobsBalance / 2 - 20);
+        bobsBalance -= 1.00;
+        bobsBalance *= 2.00;
+        bobsBalance += 1.00;
+
+        System.out.printf("Bob's new balance: %.2f%n", bobsBalance);
+    }
+}
